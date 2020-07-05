6a861ca40c981f3c5ec6b005249a966b8c8ef9f7
shivam0071kumar <shivam0071kumar@gmail.com>
2020-07-05 09:58:26 +0530

    initial commit

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,6 +1,6 @@
 
 (function() {
-    var names = ["Shivam", "Sam", "Suraj", "Avinash", "Devansh", "Sourabh", "Sounava", "Ankur", "Aditya", "Pranav"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
